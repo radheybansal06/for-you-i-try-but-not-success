36181e5703ef649216b91cc1e93d56db4093d81a
radheybansal06 <radheybansal06@gmail.com>
2025-07-14 08:14:41 +0000

    add love spin wheel app

--- love_spin_wheel.py
+++ love_spin_wheel.py
@@ -1,1 +1,22 @@
 
+import streamlit as st
+import random
+st.set_page_config(page_title="lets spin the wheel love!!!!", page_icon="🐢", layout="centered")
+st.markdown("<h1 style='text-align: center; color: pastel pink ;'>🎡 Spin the Love Wheel 💖</h1>", unsafe_allow_html=True)
+tasks =[
+    "give me virtual hug",
+    "kiss me on cheekss😘",
+    "grab my waist",
+    "do you want head",
+    "movie date tortoise🐢",
+    "send shirtless hot pic",
+    "lets talk on video call",
+    "give me 5 names by which you will gonna call me❤️",
+    "send me voice note in sexy voice😚",
+    "give me a bold lip kisssss🥺"
+]
+if st.button("spin it and lick me😁"):
+    selected=random.choice(tasks)
+    st.markdown("<h2 style='text-align: center; color: blue ;'>{selected}</h2>", unsafe_allow_html=True)
+else:
+    st.markdown("<p style='text-align: center; color: red ;'>surprise tasks are waiting for you and me tooo🥺</p>", unsafe_allow_html=True)
